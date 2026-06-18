014a4d68095af421b171c1796e2e479e66904d59
Brinda Dorairaj <brinda.dorairaj25@gmail.com>
2026-06-18 23:37:15 +0530

    Added starter code for python hackathon - simulated annealing

---
+++ Day2/genetic_algo/main.py
@@ -0,0 +1,10 @@
+from fitness import score_guess
+PASSWORD_LENGTH = 8
+ALLOWED_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz'
+
+def guess_password():
+    # write code to guess secret password (exacly 8 characters long, lowercase letters only) 
+    # use score_guess() as many times to check if password is correct 
+    # Scores range from 0 to 100 - Higher the score the better the guess
+    #  return final password when score = 100
+    return ''
