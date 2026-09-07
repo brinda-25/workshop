2f11ff9252106bfaac1f670f285bc7b7be20e14f
Brinda Dorairaj <brinda.dorairaj25@gmail.com>
2026-09-07 22:40:16 +0530

    added hints

--- genetic_algo/main.py
+++ genetic_algo/main.py
@@ -12,3 +12,21 @@ def guess_password():
 if __name__ == '__main__':
     password = guess_password()
     print(f'The password is {password}')
+
+
+## Hint 1 - Choose a random number from 0 to 7
+##  import random
+##  random_index = random.randint(0, PASSWORD_LENGTH-1)
+##
+## Hint 2 - Choose a random character from a-to-z
+##  random_character = random.choice(ALLOWED_CHARACTERS)
+##
+## Hint 3 - Convert a string to a list
+##  some_string = 'abcdefgh'
+##  some_list = list(some_string)
+##  print(some_list) --> outputs --> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',]
+##
+## Hint 4 - Merge a list of characters back into a string
+##  some_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',]
+##  some_string = ''.join(some_list)
+##  print(some_string) --> outputs --> 'abcdefgh'
